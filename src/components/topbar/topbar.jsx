8740fd562b9df443358e60d9177f74e48db3d0e5
Kenan Kadic <ken.kadic@outlook.com>
2021-10-22 11:47:52 -0500

    right side colum updated

--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -10,16 +10,19 @@ export default function Topbar() {
                     <a href="#intro" className="logo">genius.</a>
                         <div className="itemContainer">
                         <Person className="icon" />
-                        <span>314 737 1622</span>
+                            <span>(314) 737-1622</span>
                         </div>
                         <div className="itemContainer">
                         <Mail className="icon" />
-                        <span>ken.kadic@outlook.com</span>
+                            <span>ken.kadic@outlook.com</span>
                         </div>
                     </div>
-                
                     <div className="right">
-                    
+                    <div className="hamburger">
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
                 </div>
             </div>
         </div>
