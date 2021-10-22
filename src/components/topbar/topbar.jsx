3b5c636e2d022935cc26b3ee2a6ecdbb51eb9fc0
Kenan Kadic <ken.kadic@outlook.com>
2021-10-22 10:45:43 -0500

    Flexbox added

--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -2,8 +2,16 @@ import "./topbar.scss";
 
 export default function Topbar() {
     return (
-        <div className="topbar">
-          
+        <div className="topbar" id="topbar">
+            <div className="wrapper">   
+                <div className="left">
+                    <a href="#intro">assad</a>
+                </div>
+                
+                <div className="right">
+
+                </div>
+            </div>
         </div>
     )
 }
