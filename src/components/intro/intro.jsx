41bf1fdbae6facec8821f81e46b9ce68f1151be8
Kenan Kadic <ken.kadic@outlook.com>
2021-10-22 18:18:24 -0500

    images added

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -3,7 +3,21 @@ import "./intro.scss";
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-          
+          <div className="left">
+            <div className="imageContainer">
+              <img src="assets/man.png" alt="" />
+            </div>
+            </div>
+          <div className="right">
+            <div className="wrapper">
+              <h2>Hi There, I am</h2>
+              <h1>Kenan Kadic</h1>
+              <h3>Front-End Web Developer<span></span></h3>
+            </div>
+            <a href="#portfolio">
+              <img src="assets/down.png" alt="" />
+            </a>
+        </div>
         </div>
     )
 }
