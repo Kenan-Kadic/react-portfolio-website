2f9637b8e7f0ea2494ece51c3590acb7ad587402
Kenan Kadic <ken.kadic@outlook.com>
2021-10-24 20:13:56 -0500

    works page updated

--- src/components/works/works.jsx
+++ src/components/works/works.jsx
@@ -1,64 +1,85 @@
+import { useState } from "react";
 import "./works.scss";
 
 export default function Works() {
-    const data = [
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const data = [
+    {
+      id: "1",
+      icon: "./assets/mobile.png",
+      title: "Web Design",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      img:
+        "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
+    },
+    {
+      id: "2",
+      icon: "./assets/mobile.png",
+      title: "Mobile Application",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img:
+        "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
+    },
+    {
+      id: "3",
+      icon: "./assets/mobile.png",
+      title: "Branding",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img:
+        "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
+    },
+  ];
 
-        {
-            id: 1,
-            icon: "./assets/phone.png",
-            title: "Testing 1",
-            desc: "Lorem ipsum dolor sit, amet consectetur.",    
-            img: "assets/coming.png"
-        
-        },
-        {
-            id: 2,
-            icon: "./assets/phone.png",
-            title: "Testing 2",
-            desc: "Lorem ipsum dolor sit, amet consectetur.",    
-            img: "assets/coming.png"
-        
-        },
-        {
-            id: 3,
-            icon: "./assets/phone.png",
-            title: "Testing 3",
-            desc: "Lorem ipsum dolor sit, amet consectetur.",    
-            img: "assets/coming.png"
-        },
-    ];
-
-
-    return (
-        <div className="works" id="works">
-            <div className="slider">
-                {data.map((d) => (
-                <div className="container">
-                    <div className="item">
-                    <div className="left">
-                        <div className="leftContainer">
-                            <div className="imgContainer">
-                                <img src={d.icon} alt="" />
-                            </div>
-                            <h2>{d.title}</h2>
-                            <p>
-                                {d.desc} 
-                            </p>
-                            <span>Projects</span>
-                        </div>
-                    </div>
-                    <div className="right">
-                    
-                        <img src="https://cdn.dribbble.com/users/8648/screenshots/5824634/jeffsum.png?compress=1&resize=400x300" alt="" />
-                    </div>
+  const handleClick = (way) => {
+    way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+  };
+  
+  return (
+    <div className="works" id="works">
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
+        {data.map((d) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <img src={d.icon} alt="" />
+                  </div>
+                  <h2>{d.title}</h2>
+                  <p>{d.desc}</p>
+                  <span>Projects</span>
                 </div>
-                ))
-                </div> }
-                            
-            </div> 
-            
-            <img src="assets/arrow.png" className="arrow left" alt="" />
-            <img src="assets/arrow.png" className="arrow right" alt="" />
-        </div>
-    );
+              </div>
+              <div className="right">
+                <img
+                  src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <img
+        src="assets/arrow.png"
+        className="arrow left"
+        alt=""
+        onClick={() => handleClick("left")}
+      />
+      <img
+        src="assets/arrow.png"
+        className="arrow right"
+        alt=""
+        onClick={() => handleClick()}
+      />
+    </div>
+  );
 }
