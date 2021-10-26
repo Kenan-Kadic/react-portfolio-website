fd5f1f11e6c3553d97c27c01bc9b91e3c96a36d9
Kenan Kadic <ken.kadic@outlook.com>
2021-10-26 18:20:15 -0500

    contact page added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,16 @@ import "./contact.scss";
 export default function Contact() {
     return (
         <div className="contact" id="contact">
-           
+            <div className="left">
+                <img src="assets/shake.svg" alt="" />
+            </div>
+            <div className="right"></div>
+                <h2>Contact.</h2>
+                <form>
+                    <input type="text" placeholder="Email"/>
+                    <textarea placeholder="Message"></textarea>
+                    <buttom type="Submit"></buttom>
+                </form>        
         </div>
     )
 }
--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -11,11 +11,11 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
                     <a href="#intro" className="logo">Kenan Kadic</a>
                         <div className="itemContainer">
                         <Person className="icon" />
-                            <span>(314) 737-1622</span>
+                            <span>(314) 737-xxxx</span>
                         </div>
                         <div className="itemContainer">
                         <Mail className="icon" />
-                            <span>ken.kadic@outlook.com</span>
+                            <span>kenanski.kadic@outlook.com</span>
                         </div>
                     </div>
                     <div className="right">
