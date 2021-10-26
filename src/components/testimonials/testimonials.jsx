383fadbc33b45e4fe9f95f15542f179702161d19
Kenan Kadic <ken.kadic@outlook.com>
2021-10-26 08:10:40 -0500

    testimonials page created

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -3,7 +3,15 @@ import "./testimonials.scss";
 export default function Testimonials() {
     return (
         <div className="testimonials" id="testimonials">
-       
+            <h1>Testimonials</h1>
+            <div className="container"></div>
+                <div className="card"></div>
+                <div className="top"></div>
+                <img src="assets/right-arrow.png" className="left" alt="" />
+                <div className="center"></div>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                Consequuntur beatae totam illum aut veritatis.
+                <div className="bottom"></div>
         </div>
     )
 }
