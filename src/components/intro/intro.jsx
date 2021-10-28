baa8e9f304a5c9c06b4253e222ce53decb65ba99
Kenan Kadic <ken.kadic@outlook.com>
2021-10-28 08:28:11 -0500

    intro page animation updated

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -11,7 +11,7 @@ export default function Intro() {
           showCursor: true,
           backDelay: 1500,
           backSpeed: 60,
-          strings: ["React", "JavaScript", "NodeJS", "TypeScript", "HTML", "CSS", "SASS"],
+          strings: ["React", "JavaScript", "NodeJS", "TypeScript", "HTML", "CSS", "Sass"],
         });
     }, [])
 
