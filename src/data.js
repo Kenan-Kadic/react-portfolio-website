23bca547374378dc897c5f20ca6f715860e07472
Kenan Kadic <ken.kadic@outlook.com>
2021-11-01 11:57:56 -0500

    portfolio page updated

--- src/data.js
+++ src/data.js
@@ -3,19 +3,19 @@ export const featuredPortfolio = [
       id: 1,
       title: "Code with Kenan",
       img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+        "/assets/codewithkenan.png",
     },
     {
       id: 2,
-      title: "Flagship Phone Website",
+      title: "Flagship Phones",
       img:
-        "https://cdn.dribbble.com/users/702789/screenshots/15054318/media/4ea5d492b7b07eebc9528ff960794879.png?compress=1&resize=1200x900",
+        "/assets/flagship.png",
     },
     {
       id: 3,
       title: "Collab Blog",
       img:
-        "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
+      "/assets/blog.png",
     },
   
   ];
@@ -24,7 +24,8 @@ export const featuredPortfolio = [
     {
       id: 1,
       title: "Code with Kenan",
-      img: "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
+      img: 
+      "/assets/codewithkenan.png",
     },
     {
       id: 2,
@@ -44,9 +45,9 @@ export const featuredPortfolio = [
   export const javaPortfolio = [
     {
       id: 1,
-      title: "Flagship Phone Website",
+      title: "Flagship Phones",
       img:
-        "https://cdn.dribbble.com/users/2597268/screenshots/15468893/media/44313553d73ba41580f1df69749bba28.jpg?compress=1&resize=1200x900",
+      "/assets/flagship.png",
     },
     
     {
@@ -69,7 +70,7 @@ export const featuredPortfolio = [
       id: 1,
       title: "Collab Blog",
       img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+      "/assets/blog.png",
     },
     {
       id: 2,
