8d00549b3efdcad60f648d0d10ba24c5727bdedd
Kenan Kadic <ken.kadic@outlook.com>
2021-11-04 22:02:10 -0500

    a text color changed on portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -65,13 +65,13 @@ export default function Portfolio() {
       <div className="container">
         {data.map((d) => (
           <div className="item">
-            <a href={d.link}>
+            
             <img
               src={d.img}
               alt=""
             />
-            </a>
-            <h3>{d.title}</h3>
+            
+            <h3><a href={d.link}>{d.title}</a></h3>
             
           </div>
         ))}
