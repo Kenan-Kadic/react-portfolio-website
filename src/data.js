47c431ec3a3ab18a78b3ce46d0178a3c482030fa
Kenan Kadic <ken.kadic@outlook.com>
2021-11-08 16:46:35 -0600

    image name change

--- src/data.js
+++ src/data.js
@@ -10,7 +10,7 @@ export const featuredPortfolio = [
       id: 2,
       title: "Nurse Blog (in progress)",
       img:
-        "/assets/nurseblog2.png",
+        "/assets/nurseblog2.PNG",
       link: "http://www.blogwithnurses.com"
     },
     {
@@ -34,7 +34,7 @@ export const featuredPortfolio = [
       id: 2,
       title: "Nurse Blog (in progress)",
       img: 
-      "/assets/nurseblog2.png",
+      "/assets/nurseblog2.PNG",
       link: "http://www.blogwithnurses.com"
     },
     {
